80d792b4228666b53ba0de97db058baeebc0da1b
Siddhant Bedre <siddhantbedre2004@gmail.com>
2024-07-30 23:46:46 +0530

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+//Name - Siddhant Bedre
+//PRN - 23070123037
+//Aim - Printing out Hello World
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World";
+    return 0;
+}
+/* Output:
+Hello World
+*/
